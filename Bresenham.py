ba07dd7a4139f4dcb0c253a3f4e4464895b072f0
lambert remi <remi.lambert1@phelma.grenoble-inp.fr>
2023-03-05 18:50:25 +0100

    a debugger sur z

--- Bresenham.py
+++ Bresenham.py
@@ -24,60 +24,28 @@ tracerSegment(0,1,4,3,pixel)
 #print(pixel)
 
 def tracerPixel3D(y, x, z, pixel):
-    pixel[x,y,z] = z + 1
+    max = np.shape(pixel)[2]
+    pixel[x,y,z] = max - z
     pixel_modif = pixel
     return pixel_modif
 
 def bresenham3D(x1, y1, z1, x2, y2, z2, pixel): #x1,y1,z1 = point de départ, x2-1,y2-1,z2-1 = point d'arrivée
     #x lines, y columns, z depth
-    dx = x2-x1
-    dy = y2-y1
-    dz = z2-z1
-    sx = 1 if x2>x1 else -1
-    sy = 1 if y2>y1 else -1
-    sz = 1 if z2>z1 else -1
-    if dx >= dy and dx >= dz:
-        err1 = dy
-        err2 = dz
-        while x1 != x2:
-            tracerPixel3D(x1, y1, z1, pixel)
-            err1 += dy
-            err2 += dz
-            if err1 >= dx:
-                y1 += sy
-                err1 -= dx
-            if err2 >= dx:
-                z1 += sz
-                err2 -= dx
-            x1 += sx
-    elif dy >= dx and dy >= dz:
-        err1 = dx
-        err2 = dz
-        while y1 != y2:
-            tracerPixel3D(x1, y1, z1, pixel)
-            err1 += dx
-            err2 += dz
-            if err1 >= dy:
-                x1 += sx
-                err1 -= dy
-            if err2 >= dy:
-                z1 += sz
-                err2 -= dy
-            y1 += sy
-    else:
-        err1 = dx
-        err2 = dy
-        while z1 != z2:
-            tracerPixel3D(x1, y1, z1, pixel)
-            err1 += dx
-            err2 += dy
-            if err1 >= dz:
-                x1 += sx
-                err1 -= dz
-            if err2 >= dz:
-                y1 += sy
-                err2 -= dz
-            z1 += sz
+    e = x2-x1
+    dx = e*2
+    dy = (y2-y1)*2
+    dz = (z2-z1)*2
+    while x2 >= x1:
+        tracerPixel3D(x1, y1, z1, pixel)
+        x1 += 1
+        e -= dy
+        if e <= 0:
+            y1 += 1
+            e += dx
+            e -= dz
+            if e <= 0:
+                z1 += 1
+                e += dy
     return (x2,y2,z2)
 
 def from3Dto2D(pixel):
@@ -89,9 +57,8 @@ def from3Dto2D(pixel):
     return pixel2D
 
 pixel1 = np.zeros((5,5,5), dtype=np.uint8)
-bresenham3D(0,0,0,5,5,5,pixel1)
-bresenham3D(3,2,0,0,4,5,pixel1)
-bresenham3D(2,1,0,3,4,2,pixel1)
+bresenham3D(0,0,0,4,4,4,pixel1)
+bresenham3D(0,0,0,4,0,4,pixel1)
 pixel2D = from3Dto2D(pixel1)
 print(pixel1)
 print("\n")
