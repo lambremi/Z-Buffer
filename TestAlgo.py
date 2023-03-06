419c2e7bbb6e9bb957d2c8987ab4fbcd70262101
Arvisucbe <guillaume.dallechallamel@sfr.fr>
2023-03-06 09:48:27 +0100

    plus simple

--- TestAlgo.py
+++ TestAlgo.py
@@ -35,132 +35,62 @@ def init():
     return master, window
 
 
-def tracerSegment(window, x1, y1, x2, y2, z1, z2, n): 
+# def tracerSegment(window, x1, y1, x2, y2, z1, z2, n): 
+#     dx = abs(x1-x2)
+#     dy = abs(y1-y2)
+#     dz = abs(z2-z1)
+    
+#     if(dx ==0):
+#         draw_pixel(window,x1,y1,z1,n)
+#         draw_pixel(window,x2,y2,z2,n)
+#     else:
+#         #detection de la granularité la plus fine
+#         #y est la plus fine (quart sup ou inf)
+#         #si point de droite a gauche, on inverse
+#         if(x2<x1):
+#             xchange = x1
+#             x1 = x2
+#             x2 = xchange
+#             ychange = y1
+#             y1 = y2
+#             y2 = ychange
+#             zchange = z1
+#             z1 = z2
+#             z2 = zchange
+#         #detction si on va vers y pos ou neg
+#         ydeplacement = 1
+#         if(y1>y2):
+#             ydeplacement = -1
+#         zdeplacement = 1
+#         if(z1>z2):
+#             zdeplacement = -1
+
+#         e = x2-x1
+#         edz = x2-x1
+
+#         while x2 >= x1:
+#             draw_pixel(window,x1,y1,z1,n)
+#             x1 += 1
+#             e -= dy
+#             edz -= dz
+#             while e <= 0 :
+#                 y1 += ydeplacement
+#                 e += dx
+#             while edz <= 0:
+#                 z1 +=zdeplacement
+#                 edz += dx
+  
+def MatriceSegment2(x1, y1 ,z1 , x2, y2, z2, m):
     dx = abs(x1-x2)
     dy = abs(y1-y2)
     dz = abs(z2-z1)
     
-    #detection de la granularité la plus fine
-    #y est la plus fine (quart sup ou inf)
-    if(dy>dx):
-        #si point de haut a bas, on inverse
-        if(y2<y1):
-            xchange = x1
-            x1 = x2
-            x2 = xchange
-            ychange = y1
-            y1 = y2
-            y2 = ychange
-            zchange = z1
-            z1 = z2
-            z2 = zchange
-
-        #detction si on va vers x pos ou neg
-        xdeplacement = 1
-        if(x1>x2):
-            xdeplacement = -1
-        zdeplacement = 1
-        if(z1>z2):
-            zdeplacement = -1
-
-        e = y2-y1
-        edz = y2-y1
-
-        while y2 >= y1:
-            draw_pixel(window,x1,y1,z1,n)
-            y1 += 1
-            e -= dx
-            edz -= dz
-            if e <= 0:
-                x1 += xdeplacement
-                e += dy
-            while edz <= 0:
-                z1 +=zdeplacement
-                edz += dy
-                
-    #x est la plus fine (quart gauche ou droit)
+    if(dx ==0):
+        m[x1][y1]=z1
+        m[x2][y2]=z2
     else:
-        #si point de droite a gauche, on inverse
-        if(x2<x1):
-            xchange = x1
-            x1 = x2
-            x2 = xchange
-            ychange = y1
-            y1 = y2
-            y2 = ychange
-            zchange = z1
-            z1 = z2
-            z2 = zchange
-        #detction si on va vers y pos ou neg
-        ydeplacement = 1
-        if(y1>y2):
-            ydeplacement = -1
-        zdeplacement = 1
-        if(z1>z2):
-            zdeplacement = -1
-
-        e = x2-x1
-        edz = x2-x1
-
-        while x2 >= x1:
-            draw_pixel(window,x1,y1,z1,n)
-            x1 += 1
-            e -= dy
-            edz -= dz
-            if e <= 0:
-                y1 += ydeplacement
-                e += dx
-            while edz <= 0:
-                z1 +=zdeplacement
-                edz += dx
-    return (x2,y2)
-
-
-def MatriceSegment(x1, y1 ,z1 , x2, y2, z2, m): 
-    dx = abs(x1-x2)
-    dy = abs(y1-y2)
-    dz = abs(z2-z1)
-    
-    #detection de la granularité la plus fine
-    #y est la plus fine (quart sup ou inf)
-    if(dy>dx):
-        #si point de haut a bas, on inverse
-        if(y2<y1):
-            xchange = x1
-            x1 = x2
-            x2 = xchange
-            ychange = y1
-            y1 = y2
-            y2 = ychange
-            zchange = z1
-            z1 = z2
-            z2 = zchange
-
-        #detction si on va vers x pos ou neg
-        xdeplacement = 1
-        if(x1>x2):
-            xdeplacement = -1
-        zdeplacement = 1
-        if(z1>z2):
-            zdeplacement = -1
-
-        e = y2-y1
-        edz = y2-y1
-
-        while y2 >= y1:
-            m[x1][y1]=z1
-            y1 += 1
-            e -= dx
-            edz -= dz
-            if e <= 0:
-                x1 += xdeplacement
-                e += dy
-            while edz <= 0:
-                z1 +=zdeplacement
-                edz += dy
-                
-    #y est la plus fine (quart gauche ou droit)
-    else:
+        #detection de la granularité la plus fine
+        #y est la plus fine (quart sup ou inf)
         #si point de droite a gauche, on inverse
         if(x2<x1):
             xchange = x1
@@ -188,13 +118,13 @@ def MatriceSegment(x1, y1 ,z1 , x2, y2, z2, m):
             x1 += 1
             e -= dy
             edz -= dz
-            if e <= 0:
+            while e <= 0 :
                 y1 += ydeplacement
                 e += dx
             while edz <= 0:
                 z1 +=zdeplacement
                 edz += dx
-    return (x2,y2)
+
 
 def traceFacette(window,x1,y1,z1,x2,y2,z2,x3,y3,z3,n):
     # Initialisation de la matrice
@@ -203,35 +133,35 @@ def traceFacette(window,x1,y1,z1,x2,y2,z2,x3,y3,z3,n):
         m[i] = [-1] * N
     
     #tracage des 3 segments
-    MatriceSegment(x1,y1,z1,x2,y2,z2,m)
-    MatriceSegment(x3,y3,z3,x2,y2,z2,m)
-    MatriceSegment(x1,y1,z1,x3,y3,z3,m)
+    MatriceSegment2(x1,y1,z1,x2,y2,z2,m)
+    MatriceSegment2(x3,y3,z3,x2,y2,z2,m)
+    MatriceSegment2(x1,y1,z1,x3,y3,z3,m)
 
     #remplissage colone par colone
-    for h in range(N):
+    # for h in range(N):
 
-        #trouver le x le plus petit, puis le plus grand
-        y1 = -1
-        y2 = -1
-        for i in range(N):
-            if(m[h][i]!=-1 and y1 != -1):
-                y2 = i
-                z2 = m[h][i]
-            if(m[h][i]!=-1 and y1 == -1):
-                y1 = i
-                z1 = m[h][i]
-        #remplissage y
-        if(y1 != -1 and y2 != -1):
-            dy = y2 - y1
-            dz = z2-z1
-            e = y2-y1
-            while y2>=y1:
-                m[h][y1] = z1
-                y1 += 1
-                e -= dz
-                while e<=0:
-                    z1 +=1
-                    e +=dy
+    #     #trouver le x le plus petit, puis le plus grand
+    #     y1 = -1
+    #     y2 = -1
+    #     for i in range(N):
+    #         if(m[h][i]!=-1 and y1 != -1):
+    #             y2 = i
+    #             z2 = m[h][i]
+    #         if(m[h][i]!=-1 and y1 == -1):
+    #             y1 = i
+    #             z1 = m[h][i]
+    #     #remplissage y
+    #     if(y1 != -1 and y2 != -1):
+    #         dy = y2 - y1
+    #         dz = z2-z1
+    #         e = y2-y1
+    #         while y2>=y1:
+    #             m[h][y1] = z1
+    #             y1 += 1
+    #             e -= dz
+    #             while e<=0:
+    #                 z1 +=1
+    #                 e +=dy
 
     draw_matrice(window,m,n)
 
@@ -298,27 +228,3 @@ if __name__ == "__main__":
 
 
 
-def tracerSegment2(window, x1, y1, x2, y2, n): 
-    ydeplacement = 1
-    if(x2<x1):
-        xchange = x1
-        x1 = x2
-        x2 = xchange
-
-    if(y1>y2):
-        ydeplacement = -1
-        dy = (-y2+y1)*2
-    else: 
-        dy = (y2-y1)*2
-    
-    e = x2-x1
-    dx = e*2
-    
-    while x2 >= x1:
-        draw_pixel(window,x1,y1,n)
-        x1 += 1
-        e -= dy
-        if e <= 0:
-            y1 += ydeplacement
-            e += dx
-    return (x2,y2)
